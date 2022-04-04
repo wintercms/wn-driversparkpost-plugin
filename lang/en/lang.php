75c8b236af99155349aab19b2e29a58c202c7b0f
Marc Jauvin <marc.jauvin@gmail.com>
2022-04-04 13:56:27 -0400

    add french localizations

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,12 @@
 <?php
 
-$name = 'Sparkpost secret';
-
 return [
     'plugin_description' => 'Sparkpost mail driver plugin',
 
     'fields' => [
         'sparkpost_secret' => [
-            'label' => $name,
-            'comment' => 'Enter your ' . $name,
+            'label' => 'Sparkpost secret',
+            'comment' => 'Enter your Sparkpost secret',
         ],
     ],
 ];
---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'plugin_description' => "Plugin qui ajoute l'envoi de courriel à travers Sparkpost",
+
+    'fields' => [
+        'sparkpost_secret' => [
+            'label' => 'Secret Sparkpost',
+            'comment' => 'Entrez votre secret pour Sparkpost',
+        ],
+    ],
+];
